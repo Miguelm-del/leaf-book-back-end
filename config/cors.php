a56dabb31fd6e01290dc8e91affa95478e3bd46d
Miguel Márcio <mmms2@aluno.ifal.edu.br>
2022-07-23 15:54:49 -0300

    Modify cors.php

--- config/cors.php
+++ config/cors.php
@@ -15,8 +15,8 @@ return [
     |
     */
 
-    'paths' => ['api/*', 'sanctum/csrf-cookie'],
-
+    // 'paths' => ['api/*', 'sanctum/csrf-cookie'],
+    'paths' => ['api/*'],
     'allowed_methods' => ['*'],
 
     'allowed_origins' => ['*'],
