ca67d3e49b31775bb006ab461395f232f2e5d919
Miguel Márcio <mmms2@aluno.ifal.edu.br>
2022-09-25 20:07:22 -0300

    Alterado metodo search de posts

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -42,29 +42,6 @@ class PostController extends Controller
         return response()->json(["result" => "ok"], 201);
     }
 
-
-
-    // public function search(Request $request){
-
-    //     $query = Review::query();
-
-    //     if ($s = $request->input('s')) {
-    //         $query->where('writer','regexp',"/.*$s/i")
-    //         ->orWhere('title_book','regexp',"/.*$s/i");
-
-    //         return $query->get();
-
-
-
-    //     } else {
-    //         return response()->json([
-    //             'notice' => 'Não foi possivel realizar a busca'
-    //         ]);
-    //     }
-
-
-    // }
-
     public function search(Request $request)
     {
 
